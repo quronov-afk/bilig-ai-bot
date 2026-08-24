7523578febd01ef4dec65848cbf218b815c8c6f7
quronov-afk <quronov@gmail.com>
2026-08-24 23:31:44 +0500

    Create states.py

---
+++ states.py
@@ -0,0 +1,34 @@
+from aiogram.fsm.state import State, StatesGroup
+
+class Access(StatesGroup):
+    waiting_for_code = State()
+
+class Registration(StatesGroup):
+    waiting_for_parent_code = State()
+
+class ParentSettings(StatesGroup): 
+    waiting_for_custom_rate = State()
+    waiting_for_child_age = State()
+    waiting_for_coin_deduction = State()
+
+class PlanCreation(StatesGroup):
+    waiting_for_child = State()
+    waiting_for_name = State()
+    waiting_for_prize = State()
+    waiting_for_deadline = State()
+    waiting_for_book_text = State()
+    waiting_for_book_photo = State()
+
+class AITestCreation(StatesGroup):
+    waiting_for_page_photo = State()
+
+class ChildReading(StatesGroup):
+    waiting_for_page_photo = State()
+    waiting_for_audio = State()
+
+class StoreSettings(StatesGroup):
+    waiting_for_item_name = State()
+    waiting_for_item_price = State()
+
+class Feedback(StatesGroup):
+    waiting_for_message = State()
