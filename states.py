893f09a297b567c0c7c9b286e14bbccbe891ea94
quronov-afk <quronov@gmail.com>
2026-08-25 11:16:11 +0500

    Update states.py

--- states.py
+++ states.py
@@ -8,6 +8,7 @@ class ParentSettings(StatesGroup):
     waiting_for_child_age = State()
     waiting_for_coin_deduction = State()
     waiting_for_coin_addition = State()
+    waiting_for_parent_pin = State()
 
 class PlanCreation(StatesGroup):
     waiting_for_child = State()
@@ -16,12 +17,24 @@ class PlanCreation(StatesGroup):
     waiting_for_book_text = State()
     waiting_for_book_photo = State()
 
+class PlanEditing(StatesGroup):
+    waiting_for_new_name = State()
+    waiting_for_new_prize = State()
+    waiting_for_new_child = State()
+
 class AITestCreation(StatesGroup):
     waiting_for_page_photo = State()
 
 class ChildReading(StatesGroup):
     waiting_for_page_photo = State()
     waiting_for_audio = State()
+    waiting_for_manual_page = State()
+
+class BolaxonaMode(StatesGroup):
+    waiting_for_pin_to_exit = State()
+
+class ManualBookOverride(StatesGroup):
+    waiting_for_manual_title = State()
 
 class StoreSettings(StatesGroup):
     waiting_for_item_name = State()
